5b0b36f96f3554a4b1b0ad652d661ffcda63dd22
HabiburRahmanAnik <habib264521@gmail.com>
2022-05-03 14:33:36 +0600

    modify

--- src/UI/LoadingSpinner.js
+++ src/UI/LoadingSpinner.js
@@ -1,9 +1,11 @@
-import classes from './LoadingSpinner.module.css'
+import classes from './LoadingSpinner.module.css';
 
 const LoadingSpinner = () => {
   return (
-    <div className={classes.loader}></div>
-  )
-}
+    <div className={classes.container}>
+      <div className={classes.loader}></div>
+    </div>
+  );
+};
 
-export default LoadingSpinner
+export default LoadingSpinner;
--- src/components/Pages/Login.js
+++ src/components/Pages/Login.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import SocialMediaLogin from '../SocialMediaLogin/SocialMediaLogin';
 import './Login.css';
